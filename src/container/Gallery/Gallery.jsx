69c7ac86c0d91f66b011cb7a68b2e4b9671a6a8f
Chinwenma <okoriechinwe1327@gmail.com>
2023-06-29 07:13:39 -0800

    gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { SubHeading, } from "../../components";
 
 import './Gallery.css';
 
 const Gallery = () => (
   <div>
-    Gallery
+      <div className='app__wrapper_info'>
+      <SubHeading title="Instagram" />
+      <h1 className='headtext__cormorant'>Photo Gallery</h1>
+      </div>
   </div>
 );
 
