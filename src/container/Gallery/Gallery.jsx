edb4b8d96e06dae99d910bd27fa4e538643ef2e2
Chinwenma <okoriechinwe1327@gmail.com>
2023-06-29 08:11:12 -0800

    gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { SubHeading, } from "../../components";
+import { images} from "../../constants";
 
 import './Gallery.css';
 
 const Gallery = () => (
-  <div>
-      <div className='app__wrapper_info'>
+  <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
       <SubHeading title="Instagram" />
-      <h1 className='headtext__cormorant'>Photo Gallery</h1>
+      <h1 className='headtext__cormorant'>Photo Gallery </h1>
+      <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
       </div>
   </div>
 );
